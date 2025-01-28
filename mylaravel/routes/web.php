d1221fa22e8e9a4ab5c4249fe77e0b3a5082cb88
66160108-camp-66 <66160108@go.buu.ac.th>
2025-01-28 14:51:05 +0700

    laravel - from

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
+
+Route::get('/mycontroller/{id?}',
+    [MyController::class, 'myfunction']);
+
+Route::post('/mycontroller/{id?}',
+    [MyController::class, 'myfunction']);
+
+
+
+Route::get('/hello{/id?}',
+    function ($val =""){
+    return "<h1>Hello World! $val</h1>";
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return "<h1>Hello</h1>";
-});
